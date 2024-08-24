5651536bcb610c57256db80d956ebd4b77059ced
Muthyalu1236 <muthyalu1236@gmail.com>
2024-08-24 17:26:26 +0530

    Foodiee backend

---
+++ Foodiee/src/test/java/com/foodiee/FoodieeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.foodiee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodieeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
